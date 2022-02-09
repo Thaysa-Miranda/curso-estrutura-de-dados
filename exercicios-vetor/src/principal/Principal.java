0a5e1506588704a8feefe003abd3fe40a19bb40c
Thaysa-Miranda <thaysa.mbmiranda@gmail.com>
2022-02-08 21:12:28 -0300

    feat: adicionando class Aluno

---
+++ exercicios-vetor/src/principal/Principal.java
@@ -0,0 +1,25 @@
+package principal;
+
+import modelo.*;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Aluno aluno1 = new Aluno("001", "Carlos Alberto");
+		Aluno aluno2 = new Aluno("002", "Maria Fernanda");
+		Aluno aluno3 = new Aluno("003", "Luana Ferreira");
+		Aluno aluno4 = new Aluno("004", "Luiza dos Santos");
+
+		/*
+		 * // Vetor vetor = new Vetor(10); // // vetor.adiconarAluno(aluno4); //
+		 * vetor.adiconarAluno(aluno1); // vetor.adiconarAluno(aluno2); // //
+		 * vetor.adicionaQualquerPosicao(aluno4, 2); // //
+		 * System.out.println(vetor.retornaAluno(0)); // //
+		 * System.out.println("O(A) aluno(a) existe no vetor?" + " " +
+		 * vetor.contemAluno(aluno4)); // // System.out.println(vetor.toString());
+		 */	
+		
+	}
+
+}
